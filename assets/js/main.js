e19273f61d6cc8ec457dbe8384ddab2cfa6a89ee
Giovanni TAFE <20114918@tafe.wa.edu.au>
2024-06-11 07:19:10 +0800

    fix: adjust responsive header

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,4 +3,6 @@ let menuToggle = document.getElementById('menuToggle')
 
 menuToggle.addEventListener('click', () => {
     mainMenu.classList.toggle('hidden')
+    mainMenu.classList.toggle('flex')
+    mainMenu.classList.toggle('gap-4')
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,13 @@ module.exports = {
         'wrapper': '1200px',
       }
     },
+    screens: {
+      'sm': '540px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
   },
   plugins: [],
 }
